033d347b271e6bf736925f6e4517a6e84bb18580
Angela Wolff <warriorwomenblog@gmail.com>
2018-07-30 17:08:38 +0100

    Section 6 complete - Connecting PSQL and Ruby

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,9 @@
+require 'pg'
+
 class Bookmark
   def self.all
-    [
-      "https://medium.com",
-      "https://pleated-jeans.com",
-      "https://www.sandimetz.com"
-    ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM bookmarks")
+    result.map { |bookmark| bookmark['url'] }
   end
 end
